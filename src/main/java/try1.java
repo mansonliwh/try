8d477bad8509f0a0a18335ad84c28804a705e107
weihangli <mansonlwhang@163.com>
2021-01-05 11:15:06 +0800

    first try: create project

---
+++ src/main/java/try1.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.io.*;
+public class try1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int i = Integer.parseInt(str);
+        System.out.println("");
+    }
+
+}
